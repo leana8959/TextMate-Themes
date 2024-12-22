383059a512f7a01b68828cd647402b64d945c0ca
Léana 江 <leana.jiang+git@icloud.com>
2024-12-22 12:48:55 +0100

    generate svg README

--- generate.sh
+++ generate.sh
@@ -4,9 +4,14 @@ set -euo pipefail
 
 mkdir -p svgs
 
+markdown=$(mktemp)
+
 for theme in tmThemes/*; do
 	theme_name=$(basename "$theme")
-	out="svgs/${theme_name%.tmTheme}.svg"
+	out="${theme_name%.tmTheme}.svg"
 	echo "Generating $theme_name..."
-	typst compile --input theme="$theme" main.typ "$out"
+	echo "![$theme_name]($out)" >> "$markdown"
+	typst compile --input theme="$theme" main.typ "svgs/$out"
 done
+
+mv "$markdown" svgs/README.md
